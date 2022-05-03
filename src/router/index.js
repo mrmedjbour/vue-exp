60f4922ae16993c6248fabb3ce7c5b6dad09bce2
MEDJBOUR MOHAMMED <me@medjbour.me>
2022-05-03 16:28:22 +0100

    x1

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import EventView from "../views/EventView.vue";
+import EventList from '../views/EventList.vue'
+import EventDetails from '../views/Event/Details.vue'
+import About from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList,
+    props: route => ({ page: parseInt(route.query.page) || 1 })
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/event/:id',
-      name: 'event',
-      props: true,
-      component: EventView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
